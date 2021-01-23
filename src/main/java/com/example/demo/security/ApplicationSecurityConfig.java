acc408f89f63a8b992e5f532dff537961d94a3a0
noza0727 <shahnoza072@gmail.com>
2021-01-23 16:36:53 +0500

    [ADDED] Form Login, default courses page and logout button

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationEnumRole.*;
@@ -33,12 +36,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http                        // new configuration
-                //.csrf().disable()// only enabled when using with real clients
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable()// only enabled when using with real clients
+                /*.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()*/
                 .authorizeRequests()// authorize request
                 .antMatchers("/", "index", "/css/*", "/js/*")  // permit entrance to main page
-                .permitAll()
+                    .permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name()) //only student can access, no admin
                                                                         //though student can access another student yet
 //                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
@@ -49,7 +52,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()     //any request must be authenticated
                 .authenticated()  // user must specify username and password
                 .and()
-                .httpBasic();  //mechanism enforcing authenticity of the client by using basic authentication, sign in pop up
+                //.httpBasic();  //mechanism enforcing authenticity of the client by using basic authentication, sign in pop up
+                .formLogin() //form basic authentication
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .passwordParameter("password") // if the parameter ets changed then it should also be changed in html file
+                    .usernameParameter("username") // if the parameter ets changed then it should also be changed in html file
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingVerySecured") // should be secured key
+                    .rememberMeParameter("remember-me") // if the parameter ets changed then it should also be changed in html file
+                .and()
+                .logout()
+                    .logoutUrl("/logout") //by default too
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                                    //delete this line(logoutRequestMatcher when csrf is enabled
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
